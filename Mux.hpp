30f99564716ebb1d1e37f98e01c7bbd5534c53cb
kenchen12 <kenchen12@gmail.com>
2017-11-02 07:57:41 -0200

    Mux adicionado, notar mudanças com relação aos metodos acordados no docs

---
+++ Mux.hpp
@@ -0,0 +1,14 @@
+#ifndef MUX_HPP
+#define MUX_HPP
+class Mux{
+	public:
+		Mux(int size);
+		int get_saida(int saida);
+		void set_entrada(int entrada, int valor);
+	private:
+		int tamanho;
+		int *entradas;
+
+};
+
+#endif
