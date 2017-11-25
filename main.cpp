ce8f161586c38acf45178bd43482190dc30d6e4b
gugation <33260223+gugation@users.noreply.github.com>
2017-11-25 16:24:48 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "memoriainstrucao.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    ifstream entrada;
+    entrada.open("entrada.txt", ios_base::in);
+    string linha;
+    MemoriaInstrucao m;
+    while(getline(entrada, linha)){
+        if(linha != "sair"){
+            m.set_wdata(linha);
+            m.write();
+        }
+    }
+
+    return 0;
+}
