00bce684d4fb037ffb0c29fef23e958fe27accd3
gugation <33260223+gugation@users.noreply.github.com>
2017-11-25 17:56:40 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 #include "MemoriaInstrucao.hpp"
+#include "BancoDeRegistradores.hpp"
 
 using namespace std;
 
@@ -11,12 +13,47 @@ int main(){
     entrada.open("entrada.txt", ios_base::in);
     string linha;
     MemoriaInstrucao m;
+    BancoDeRegistradores b(32);
+    int pc = 0;
     while(getline(entrada, linha)){
         if(linha != "sair"){
             m.set_wdata(linha);
+            m.set_address(pc * 4);
             m.write();
         }
+        pc++;
     }
+
+    /*int numero = 0;
+    while(numero != -1){
+
+        printf("     ___       __________    \n");
+        printf("    |   |     | Memoria  |   \n");
+        printf("    |PC |     |    de    |   \n");
+        printf("    |   |     | Instrucao|   \n");
+        printf("    |%3d|->   |          |   \n", numero);
+        printf("    |___|     |__________|   \n");
+        cin >> numero;
+        system("clear");
+        printf("     ___       __________    \n");
+        printf("    |   |     | Memoria  |   \n");
+        printf("    |PC |     |    de    |   \n");
+        printf("    |   |     | Instrucao|   \n");
+        printf("    |%3d|-->  |          |   \n", numero);
+        printf("    |___|     |__________|   \n");
+        cin >> numero;
+        system("clear");
+        printf("     ___       __________    \n");
+        printf("    |   |     | Memoria  |   \n");
+        printf("    |PC |     |    de    |   \n");
+        printf("    |   |     | Instrucao|   \n");
+        printf("    |%3d|---> |          |   \n", numero);
+        printf("    |___|     |__________|   \n");
+        cin >> numero;
+        system("clear");
+    }*/
 
     return 0;
 }
+
+// PC,
