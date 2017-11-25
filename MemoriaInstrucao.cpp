e488b4d41a24d8f656471b9dcf5e51d892edff57
gugation <33260223+gugation@users.noreply.github.com>
2017-11-25 16:37:07 -0300

    Update MemoriaInstrucao.cpp

--- MemoriaInstrucao.cpp
+++ MemoriaInstrucao.cpp
@@ -1,26 +1,26 @@
 #include "MemoriaInstrucao.hpp"
 
 using namespace std;
-Memoria::Memoria(){
-	address = 0;
-	w_data = 0;
-	for(int i = 0; i < TAM; i++)
-		dados[i] = 0;
+MemoriaInstrucao::MemoriaInstrucao(){
+    address = 0;
+    w_data = "";
+    for(int i = 0; i < TAM; i++)
+        dados[i] = "";
 }
 
-void Memoria::set_address(int val){
-	val /= 4;
-	address = val % TAM;
+void MemoriaInstrucao::set_address(int val){
+    val /= 4;
+    address = val % TAM;
 }
 
-string Memoria::read(){
-	return dados[address];
+string MemoriaInstrucao::read(){
+    return dados[address];
 }
 
-void Memoria::set_wdata(int val){
-	w_data = val;
+void MemoriaInstrucao::set_wdata(string val){
+    w_data = val;
 }
 
-void Memoria::write(){
-	dados[address] = w_data;
+void MemoriaInstrucao::write(){
+    dados[address] = w_data;
 }
