c572c43c4c68c66930e227bc8ec3d90dc61df758
gugation <33260223+gugation@users.noreply.github.com>
2017-11-25 14:50:42 -0200

    Add files via upload

---
+++ MEMWB.hpp
@@ -0,0 +1,33 @@
+#ifndef MEMWB_H
+#define MEMWB_H
+
+
+class MEMWB{
+    public:
+        MEMWB();
+
+        string getir();
+
+        int getALUOut();
+        void setALUOut(int val);
+
+        int getLMemoryData();
+        void setLMemoryData(int val);
+
+        int getRegWrite();
+        void setRegWrite(int val);
+
+        int getMemtoReg();
+        void setMemtoReg(int val);
+
+
+
+    private:
+        string ir;
+        int ALUOut;
+        int LMemoryData;
+        int RegWrite;
+        int MemtoReg;
+};
+
+#endif // MEMWB_H
