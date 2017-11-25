6db622e57f9e9b93518ec769c22bb4488e65014d
gugation <33260223+gugation@users.noreply.github.com>
2017-11-25 16:36:49 -0300

    Update MemoriaInstrucao.hpp

--- MemoriaInstrucao.hpp
+++ MemoriaInstrucao.hpp
@@ -1,20 +1,21 @@
 #ifndef MEMORIA_HPP
 #define MEMORIA_HPP
+
 #include "Registrador.hpp"
 #define TAM 1024
 #include <string>
 
-class Memoria{
-	public:
-		Memoria();
-		void set_address(int val);
-		void set_wdata(int val);
-		string read();
-		void write();
+class MemoriaInstrucao{
+    public:
+        MemoriaInstrucao();
+        void set_address(int val);
+        void set_wdata(string val);
+        string read();
+        void write();
 
-	private:
-		int address, w_data;
-		string dados[TAM];
+    private:
+        int address;
+        string dados[TAM], w_data;
 };
 
 #endif
