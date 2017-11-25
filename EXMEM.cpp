18392d48bf9827503960238d92b5f163ef862253
asdomingues <allan.domingues@usp.br>
2017-11-25 17:59:19 -0200

    corrected EXMEM, now written based on the correct diagram

--- EXMEM.cpp
+++ EXMEM.cpp
@@ -1,13 +1,16 @@
 #include <string>
 #include "EXMEM.hpp"
 
+
 EXMEM::EXMEM() {
 	this->ir = "";
-	this->branch = 0;
+	this->branch_address = 0;
+	this->alu_zero = false;
 	this->alu_out = 0;
 	this->alu_in2 = 0;
-	this->npc = 0;
+	this->write_reg_address = false;
 	this->reg_write = false;
+	this->branch = false;
 	this->mem_read = false;
 	this->mem_write = false;
 	this->mem_to_reg = false;
@@ -17,8 +20,12 @@ void EXMEM::set_ir(string ir) {
 	this->ir = ir;
 }
 
-void EXMEM::set_branch(int branch) {
-	this->branch = branch;
+void EXMEM::set_branch_address(int branch_address) {
+	this->branch_address = branch_address;
+}
+
+void EXMEM::set_alu_zero(bool alu_zero) {
+	this->alu_zero = alu_zero;
 }
 
 void EXMEM::set_alu_out(int alu_out) {
@@ -29,12 +36,16 @@ void EXMEM::set_alu_in2(int alu_in2) {
 	this->alu_in2 = alu_in2;
 }
 
-void EXMEM::set_npc(int npc) {
-	this->npc = npc;
+void EXMEM::set_write_reg_address(int write_reg_address) {
+	this->write_reg_address = write_reg_address;
 }
 
 void EXMEM::set_reg_write(bool reg_write) {
 	this->reg_write = reg_write;
+}
+
+void EXMEM::set_branch(bool branch) {
+	this->branch = branch;
 }
 
 void EXMEM::set_mem_read(bool mem_read) {
@@ -49,12 +60,17 @@ void EXMEM::set_mem_to_reg(bool mem_to_reg) {
 	this->mem_to_reg = mem_to_reg;
 }
 
-string EXMEM::get_ir(string ir) {
-	this->ir = ir;
+
+string EXMEM::get_ir() {
+	return this->ir;
 }
 
-int EXMEM::get_branch() {
-	return this->branch;
+int EXMEM::get_branch_address() {
+	return this->branch_address;
+}
+
+bool EXMEM::get_alu_zero() {
+	return this->alu_zero;
 }
 
 int EXMEM::get_alu_out() {
@@ -65,12 +81,16 @@ int EXMEM::get_alu_in2() {
 	return this->alu_in2;
 }
 
-int EXMEM::get_npc() {
-	return this->npc;
+int EXMEM::get_write_reg_address() {
+	return this->write_reg_address;
 }
 
 bool EXMEM::get_reg_write() {
 	return this->reg_write;
+}
+
+bool EXMEM::get_branch() {
+	return this->branch;
 }
 
 bool EXMEM::get_mem_read() {
@@ -83,4 +103,8 @@ bool EXMEM::get_mem_write() {
 
 bool EXMEM::get_mem_to_reg() {
 	return this->mem_to_reg;
+}
+
+int main (void) {
+	return 0;
 }
--- EXMEM.hpp
+++ EXMEM.hpp
@@ -6,36 +6,49 @@
 using namespace std;
 
 class EXMEM {
-	private:
-		string ir;
-		int branch;
-		int alu_out;
-		int alu_in2;
-		int npc;
-		bool reg_write;
-		bool mem_read;
-		bool mem_write;
-		bool mem_to_reg;
 	public:
 		EXMEM();
 		void set_ir(string ir);
-		void set_branch(int branch);
+
+		void set_branch_address(int branch_address);
+		void set_alu_zero(bool alu_zero);
 		void set_alu_out(int alu_out);
 		void set_alu_in2(int alu_in2);
-		void set_npc(int npc);
+		void set_write_reg_address(int write_reg_address);
+
 		void set_reg_write(bool reg_write);
+		void set_branch(bool branch);
 		void set_mem_read(bool mem_read);
 		void set_mem_write(bool mem_write);
 		void set_mem_to_reg(bool mem_to_reg);
+
 		string get_ir();
-		int get_branch();
+
+		int get_branch_address();
+		bool get_alu_zero();
 		int get_alu_out();
 		int get_alu_in2();
-		int get_npc();
+		int get_write_reg_address();
+
 		bool get_reg_write();
+		bool get_branch();
 		bool get_mem_read();
 		bool get_mem_write();
 		bool get_mem_to_reg();
-}
+	private:
+		string ir;
+
+		int branch_address;
+		bool alu_zero;
+		int alu_out;
+		int alu_in2;
+		int write_reg_address;
+
+		bool reg_write;
+		bool branch;
+		bool mem_read;
+		bool mem_write;
+		bool mem_to_reg;
+};
 
 #endif
