05b1f449f67e116edeeaa7ac89247fce59d94f6c
gugation <33260223+gugation@users.noreply.github.com>
2017-11-25 15:29:20 -0200

    Update Memoria.hpp

--- Memoria.hpp
+++ Memoria.hpp
@@ -2,15 +2,17 @@
 #define MEMORIA_HPP
 #include "Registrador.hpp"
 #define TAM 1024
+
 class Memoria{
 	public:
 		Memoria();
-		void set_endereco(int val);
+		void set_address(int val);
 		void set_wdata(int val);
 		int read();
 		void write();
+
 	private:
-		int adress, w_data;
+		int address, w_data;
 		int dados[TAM];
 };
 
